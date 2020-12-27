ba6283faadf370102b6bc8830f12dad13d460c00
Joseph Pasqualini <joseph.pasqualini@adde.fr>
2020-12-27 16:45:47 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import fileUpload from 'express-fileupload'
 // import { router as RealmImporterRouter } from './routes/realmImporter'
 import { PORT } from './config/constants'
-import { CharacterManager } from './managers/CharacterManager'
+import { CharacterManager } from 'delta-green-core/src/managers/CharacterManager'
 
 const app = express()
 app.use(express.json())
@@ -15,14 +15,3 @@ app.use(
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`)
 })
-
-const manager = new CharacterManager()
-
-console.log('-------------------------------')
-for (let index = 0; index < 10; index++) {
-    console.log(`${index + 1} ${JSON.stringify(manager.randomMaleCharacter())}`)
-    console.log(
-        `${index + 1} ${JSON.stringify(manager.randomFemaleCharacter())}`
-    )
-}
-console.log('-------------------------------')
