a019e733d323544b80819180c760ca2f9ca12b8a
thunderkerrigan <thunderkerrigan@gmail.com>
2021-07-26 23:03:46 +0200

    ## WIP
     * added a route for generating a randomUser

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -30,4 +30,17 @@ router.get(
   }
 );
 
+router.get(
+  "/randomCharacter",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const character = await playerManager.randomCharacter();
+      res.send(<CharacterModel>character);
+    } catch (e) {
+      console.log(e);
+      res.send(<boolean>false);
+    }
+  }
+);
+
 export default router;
